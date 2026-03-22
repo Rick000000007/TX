46b59cdcc57e7d398f821881137b998d023e9cdb
Rick000000007 <rickmajumdar2019@gmail.com>
2026-03-22 16:48:44 +0530

    Replace terminal surface with mainscreen debug text

--- android/app/src/main/java/com/tx/terminal/ui/screens/MainScreen.kt
+++ android/app/src/main/java/com/tx/terminal/ui/screens/MainScreen.kt
@@ -217,9 +217,9 @@ fun MainScreen(viewModel: TerminalViewModel) {
                         .fillMaxSize()
                         .background(Color.Magenta)
                 ) {
-                    TerminalSurface(
-                        viewModel = viewModel,
-                        modifier = Modifier.fillMaxSize()
+                    Text(
+                        text = "MAINSCREEN DEBUG",
+                        color = Color.White
                     )
                 }
                 
